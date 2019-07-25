782a05d38d417d070685aa98b77a7103c3b47e22
Chris <chrng2@gmail.com>
2019-07-25 13:12:18 -0700

    Improve New Quote button + Define randomBetween helper function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './App.css';
 
-const QUOTES = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+const QUOTES = `https://gist.githubusercontent.com/
+                camperbot/5a022b72e96c4c9585c32bf6a75f62d9/
+                raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/
+                quotes.json`;
 
 const LOADS = ['Searching...', 'Generating...', 'Thinking...', 'Asking Alexa...', 'Reading 📖',
                'Going to the library...', '💤',
                '🔎📚', '🔎🌐', '🤔'];
 
-// TODO: add collision checks to QuoteBox
+// TODO: - add collision checks to QuoteBox
+
+// DONE: ✅ make button disappear on click
+//       ✅ make get random between min and max function
+//       ✅ prettify author string
+
+const randomBetween = (min, max) => ( Math.floor( Math.random() * (max - min + 1) + min ) );
 
 class QuoteBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: 'The quick brown fox jumped over the lazy dog.',
+      quote: ' The quick brown fox jumped over the lazy dog.',
       author: '-Bob',
       quotes: [],
       load: '',
@@ -25,7 +34,7 @@ class QuoteBox extends React.Component {
   }
 
   componentDidMount() {
-    fetch(QUOTES)
+    fetch(QUOTES.replace(/\s/g, ''))
       .then( a => a.json() )
       .then( b => this.setState({ quotes: b.quotes }) );
   }
@@ -33,9 +42,11 @@ class QuoteBox extends React.Component {
   getQuote() {
     const min = 0;
     let max = this.state.quotes.length - 1;
-    const newQuote = this.state.quotes[ Math.floor(Math.random() * (max - min + 1) + min) ];
+    const newQuote = this.state.quotes[randomBetween(min, max)];
+    newQuote.quote = ' ' + newQuote.quote;
+    newQuote.author = '-' + newQuote.author.replace( /[^a-z. ]/gi, '' );
     max = this.state.loads.length - 1;
-    const newLoad = this.state.loads[ Math.floor(Math.random() * (max - min + 1) + min) ];
+    const newLoad = this.state.loads[randomBetween(min, max)];
     this.setState({
       load: newLoad
     });
@@ -49,18 +60,18 @@ class QuoteBox extends React.Component {
       } else {
         this.setState({
           quote: newQuote.quote,
-          author: '-' + newQuote.author,
+          author: newQuote.author,
           isquote: true
         })
       }
     }, 500);
+  }
+
+  clicked() {
     const card = document.querySelector('.card');
     const btn = document.querySelector('#new-quote');
     btn.classList.add('trigger');
     card.addEventListener('transitionend', () => btn.classList.remove('trigger'));
-  }
-
-  clicked() {
     this.props.changeBackground();
   }
 
@@ -68,10 +79,15 @@ class QuoteBox extends React.Component {
     return (
       <div className='grid'>
         <div id='quote-box'>
-          <button onClick={() => { this.getQuote(); this.clicked(); }} id='new-quote'>New Quote</button>
+          <button onClick={() => { this.getQuote(); this.clicked(); }} id='new-quote'>
+            New Quote
+          </button>
           <div className='card'>
             <div className='card-front'>
-              <p id='text'>{this.state.isquote ? <i className='fa fa-quote-left'></i> : ''} {this.state.quote}</p>
+              <p id='text'>
+                {this.state.isquote ? <i className='fa fa-quote-left'></i> : ''}
+                {this.state.quote}
+              </p>
               <p id='author'>{this.state.author}</p>
             </div>
             <div className='card-back'>
@@ -90,8 +106,8 @@ class Backdrop extends React.Component {
     super(props);
     this.state = {
       background: 'whitesmoke',
-      colors: ['#16A085', '#27AE60', '#2C3E50', '#F39C12', '#E74C3C',
-               '#9B59B6', '#FB6964', '#342224', '#472E32', '#BDBB99',
+      colors: ['#16A085', '#27AE60', '#F39C12', '#E74C3C',
+               '#9B59B6', '#FB6964', '#BDBB99',
                '#77B1A9', '#73A857']
 
     };
@@ -101,9 +117,9 @@ class Backdrop extends React.Component {
   changeBackground() {
     const min = 0;
     const max = this.state.colors.length - 1;
-    let newBackground = this.state.colors[ Math.floor(Math.random() * (max - min + 1) + min) ];
+    let newBackground = this.state.colors[randomBetween(min, max)];
     while (newBackground === this.state.background) {
-      newBackground = this.state.colors[ Math.floor(Math.random() * (max - min + 1) + min) ];
+      newBackground = this.state.colors[randomBetween(min, max)];
     };
     this.setState({
       background: newBackground
