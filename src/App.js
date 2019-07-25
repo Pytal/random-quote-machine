4b16876832ba3315fa7c561196711ca00ebc3255
Chris <chrng2@gmail.com>
2019-07-24 23:09:20 -0700

    More improvements + added backdrop fade + added LOADS

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 
 const QUOTES = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
+const LOADS = ['Searching...', 'Generating...', 'Thinking...', 'Asking Alexa...', 'Reading 📖',
+               'Going to the library...', '💤',
+               '🔎📚', '🔎🌐', '🤔'];
+
+// TODO: add collision checks to QuoteBox
+
 class QuoteBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: 'The quick brown fox jumped over the lazy dog',
-      author: 'Bob',
+      quote: 'The quick brown fox jumped over the lazy dog.',
+      author: '-Bob',
       quotes: [],
-      anim: ''
+      load: '',
+      loads: LOADS,
+      isquote: true
     };
     this.getQuote = this.getQuote.bind(this);
+    this.clicked = this.clicked.bind(this);
   }
 
   componentDidMount() {
@@ -23,15 +32,32 @@ class QuoteBox extends React.Component {
 
   getQuote() {
     const min = 0;
-    const max = this.state.quotes.length - 1;
+    let max = this.state.quotes.length - 1;
     const newQuote = this.state.quotes[ Math.floor(Math.random() * (max - min + 1) + min) ];
+    max = this.state.loads.length - 1;
+    const newLoad = this.state.loads[ Math.floor(Math.random() * (max - min + 1) + min) ];
     this.setState({
-      quote: newQuote.quote,
-      author: newQuote.author,
-      anim: 'trigger'
+      load: newLoad
     });
-    const elem = document.querySelector('.card');
-    elem.addEventListener('transitionend', () => this.setState({ anim: '' }));
+    setTimeout(() => {
+      if (newLoad === '💤') {
+        this.setState({
+          quote: '',
+          author: '',
+          isquote: false
+        })
+      } else {
+        this.setState({
+          quote: newQuote.quote,
+          author: '-' + newQuote.author,
+          isquote: true
+        })
+      }
+    }, 500);
+    const card = document.querySelector('.card');
+    const btn = document.querySelector('#new-quote');
+    btn.classList.add('trigger');
+    card.addEventListener('transitionend', () => btn.classList.remove('trigger'));
   }
 
   clicked() {
@@ -42,13 +68,15 @@ class QuoteBox extends React.Component {
     return (
       <div className='grid'>
         <div id='quote-box'>
-          <button onClick={() => { this.getQuote(); this.clicked(); }} className={this.state.anim} id='new-quote'>New Quote</button>
-          <div className={'card'/* + this.state.anim */}>
+          <button onClick={() => { this.getQuote(); this.clicked(); }} id='new-quote'>New Quote</button>
+          <div className='card'>
             <div className='card-front'>
-              <p id='text'><i className='fa fa-quote-left'></i> {this.state.quote}</p>
-              <p id='author'>-{this.state.author}</p>
+              <p id='text'>{this.state.isquote ? <i className='fa fa-quote-left'></i> : ''} {this.state.quote}</p>
+              <p id='author'>{this.state.author}</p>
             </div>
-            <div className='card-back'></div>
+            <div className='card-back'>
+              <p id='back-text'>{this.state.load}</p>
+            </div>
           </div>
         </div>
         <button id='tweet-quote'><i className='fa fa-twitter'></i></button>
@@ -61,7 +89,7 @@ class Backdrop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      background: '#333',
+      background: 'whitesmoke',
       colors: ['#16A085', '#27AE60', '#2C3E50', '#F39C12', '#E74C3C',
                '#9B59B6', '#FB6964', '#342224', '#472E32', '#BDBB99',
                '#77B1A9', '#73A857']
@@ -73,7 +101,10 @@ class Backdrop extends React.Component {
   changeBackground() {
     const min = 0;
     const max = this.state.colors.length - 1;
-    const newBackground = this.state.colors[ Math.floor(Math.random() * (max - min + 1) + min) ];
+    let newBackground = this.state.colors[ Math.floor(Math.random() * (max - min + 1) + min) ];
+    while (newBackground === this.state.background) {
+      newBackground = this.state.colors[ Math.floor(Math.random() * (max - min + 1) + min) ];
+    };
     this.setState({
       background: newBackground
     });
