f25d8d8bfcd22d0d2c9e14dcc4af3346527d04cb
Chris <chrng2@gmail.com>
2019-08-01 21:49:38 -0700

    Rewrite using Hooks and unstated-next

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { createContainer } from 'unstated-next';
 import './App.css';
 
-const QUOTES = `https://gist.githubusercontent.com/
-                camperbot/5a022b72e96c4c9585c32bf6a75f62d9/
-                raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/
-                quotes.json`;
+const QUOTESURL = `https://gist.githubusercontent.com/
+                   camperbot/5a022b72e96c4c9585c32bf6a75f62d9/
+                   raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/
+                   quotes.json`;
 
 const LOADS = ['Searching...', 'Generating...', 'Thinking...', 'Asking Alexa...', 'Reading 📖',
                'Going to the library...', '💤',
                '🔎📚', '🔎🌐', '🤔'];
 
+const COLOURS = ['#16A085', '#27AE60', '#F39C12', '#E74C3C',
+                 '#9B59B6', '#FB6964', '#BDBB99',
+                 '#77B1A9', '#73A857'];
+
 //       🆗 freeCC Feature Complete
+
+// TODO:
+
+// BUGS: 🔧 useEffect has a missing dependency: 'quotehooks'
 
 // DONE: ✅ make button disappear on click
 //       ✅ define randomBetween min and max function
@@ -20,7 +29,165 @@ const LOADS = ['Searching...', 'Generating...', 'Thinking...', 'Asking Alexa...'
 //       ✅ add collision checks to QuoteBox
 //       ✅ replace while loop with do...while loop in getNewValue
 //       ✅ improve quote symbol display logic
+//       ✅ implement hooks and unstated-next
 
+// Hooks
+function useFetchHooks() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function setQuotesConstant() {
+    const promise = await fetch( QUOTESURL.replace(/\s/g, '') );
+    const json = await promise.json();
+    setQuotes(json.quotes);
+    setLoading(false);
+  };
+
+  useEffect(() => setQuotesConstant(), [] );
+
+  return { quotes, loading };
+};
+
+function useQuoteHooks() {
+  const [quote, setQuote] = useState(' The quick brown fox jumped over the lazy dog.');
+  const [author, setAuthor] = useState('-Bob');
+  const loadhooks = LoadHooks.useContainer();
+  const fetchhooks = FetchHooks.useContainer();
+
+  const getQuote = () => {
+    const newQuote = getNewValue(quote, fetchhooks.quotes);
+    setTimeout(() => {
+      if (loadhooks.load === '💤') {
+        setQuote('');
+        setAuthor('');
+      } else {
+        setQuote(' ' + newQuote.quote);
+        setAuthor('-' + newQuote.author.trim().replace( /[^a-z. ]/gi, '' ));
+      };
+    }, 500);
+  };
+
+  return { quote, author, getQuote };
+};
+
+function useLoadHooks() {
+  const [load, setLoad] = useState('');
+  const getLoad = () => setLoad( load => getNewValue(load, LOADS) );
+
+  return { load, getLoad };
+};
+
+function useBackgroundHooks() {
+  const [background, setBackground] = useState('whitesmoke');
+  const getBackground = () => setBackground( getNewValue(background, COLOURS) );
+  
+  return { background, getBackground };
+};
+
+// Containers
+const FetchHooks = createContainer(useFetchHooks);
+const QuoteHooks = createContainer(useQuoteHooks);
+const LoadHooks = createContainer(useLoadHooks);
+const BackgroundHooks = createContainer(useBackgroundHooks);
+
+// Functions
+function trigger(classSelector, siblingElem, transitionElem) {
+  const card = document.querySelector(transitionElem);
+  const quoteBtn = document.querySelector(siblingElem);
+  card.addEventListener('transitionend', () => quoteBtn.classList.remove(classSelector));
+  quoteBtn.classList.add(classSelector);
+};
+
+function tweet(quote, author, classSelector, twitterLinkElem, transitionElem) {
+  const tweetLnk = document.querySelector(twitterLinkElem);
+  const tweetBtn = document.querySelector(transitionElem);
+  setTimeout( () => tweetBtn.classList.remove(classSelector), 1000 );
+  tweetBtn.classList.add(classSelector);
+  setTimeout( () => window.open( tweetLnk.href + '?&text=' + encodeURIComponent(
+                    '"' + quote.trim() + '" ' + author)
+                    ), 150 );
+};
+
+// Displays
+function QuoteBoxDisplay() {
+  const fetchhooks = FetchHooks.useContainer();
+  const quotehooks = QuoteHooks.useContainer();
+  const loadhooks = LoadHooks.useContainer();
+  const backgroundhooks = BackgroundHooks.useContainer();
+
+  // 🔧 useEffect has a missing dependency: 'quotehooks'
+  const firstLoad = useRef(true);
+  useEffect(() => {
+    if (firstLoad.current) { firstLoad.current = false; return; };
+    quotehooks.getQuote();
+  }, [loadhooks.load] );
+
+  return (
+    <div className='grid'>
+      {fetchhooks.loading ? <h1>Loading...</h1> :
+      <div id='quote-box'>
+        <button
+          id='new-quote'
+          onClick={() => {
+            loadhooks.getLoad();
+            trigger('trigger', '#new-quote', '.card');
+            backgroundhooks.getBackground();
+          }}>
+          New Quote
+        </button>
+        <div className='card'>
+          <div className='card-front'>
+            <p id='text'>
+              {quotehooks.quote !== '' ? <i className='fa fa-quote-left'></i> : ''}
+              {quotehooks.quote}
+            </p>
+            <p id='author'>{quotehooks.author}</p>
+          </div>
+          <div className='card-back'>
+            <p id='back-text'>{loadhooks.load}</p>
+          </div>
+        </div>
+        <button
+          id='tweet-quote-btn'
+          onClick={() =>
+            tweet(quotehooks.quote, quotehooks.author,
+                  'trigger', '#tweet-quote', '#tweet-quote-btn')}>
+          <i className='fa fa-twitter'></i>
+          <a id='tweet-quote' href='https://twitter.com/intent/tweet'>
+            Tweet Quote
+          </a>
+        </button>
+      </div>
+      }
+    </div>
+  )
+};
+
+function BackdropDisplay() {
+  const backgroundhooks = BackgroundHooks.useContainer();
+
+  return (
+    <div className='backdrop' style={{ backgroundColor: backgroundhooks.background }}>
+      <FetchHooks.Provider>
+      <LoadHooks.Provider>
+      <QuoteHooks.Provider>
+        <QuoteBoxDisplay />
+      </QuoteHooks.Provider>
+      </LoadHooks.Provider>
+      </FetchHooks.Provider>
+    </div>
+  )
+};
+
+function Display() {
+  return (
+    <BackgroundHooks.Provider>
+      <BackdropDisplay />
+    </BackgroundHooks.Provider>
+  )
+};
+
+// Helper Functions
 const randomBetween = (min, max) => ( Math.floor( Math.random() * (max - min + 1) + min ) );
 
 const getNewValue = (prev, arr) => {
@@ -31,123 +198,4 @@ const getNewValue = (prev, arr) => {
   return curr;
 };
 
-class QuoteBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quote: ' The quick brown fox jumped over the lazy dog.',
-      author: '-Bob',
-      quotes: [],
-      load: '',
-      loads: LOADS
-    };
-    this.getQuote = this.getQuote.bind(this);
-    this.clicked = this.clicked.bind(this);
-    this.tweeted = this.tweeted.bind(this);
-  }
-
-  componentDidMount() {
-    fetch(QUOTES.replace(/\s/g, ''))
-      .then( a => a.json() )
-      .then( b => this.setState({ quotes: b.quotes }) );
-  }
-
-  getQuote() {
-    const newQuote = getNewValue(this.state.quote, this.state.quotes);
-    newQuote.quote = ' ' + newQuote.quote;
-    newQuote.author = '-' + newQuote.author.trim().replace( /[^a-z. ]/gi, '' );
-    const newLoad = getNewValue(this.state.load, this.state.loads);
-    this.setState({
-      load: newLoad
-    });
-    setTimeout( () => {
-      if (newLoad === '💤') {
-        this.setState({
-          quote: '',
-          author: ''
-        })
-      } else {
-        this.setState({
-          quote: newQuote.quote,
-          author: newQuote.author
-        })
-      }
-    }, 500 );
-  }
-
-  clicked() {
-    const card = document.querySelector('.card');
-    const quoteBtn = document.querySelector('#new-quote');
-    card.addEventListener('transitionend', () => quoteBtn.classList.remove('trigger'));
-    quoteBtn.classList.add('trigger');
-    this.props.changeBackground();
-  }
-
-  tweeted() {
-    const tweetLnk = document.querySelector('#tweet-quote');
-    const tweetBtn = document.querySelector('#tweet-quote-btn');
-    setTimeout( () => tweetBtn.classList.remove('trigger'), 1000 );
-    tweetBtn.classList.add('trigger');
-    setTimeout( () => window.open( tweetLnk.href + '?&text=' + encodeURIComponent(
-                      '"' + this.state.quote.trim() + '" ' + this.state.author) ),
-                      150 );
-  }
-
-  render() {
-    return (
-      <div className='grid'>
-        <div id='quote-box'>
-          <button onClick={() => { this.getQuote(); this.clicked(); }} id='new-quote'>
-            New Quote
-          </button>
-          <div className='card'>
-            <div className='card-front'>
-              <p id='text'>
-                {this.state.quote !== '' ? <i className='fa fa-quote-left'></i> : ''}
-                {this.state.quote}
-              </p>
-              <p id='author'>{this.state.author}</p>
-            </div>
-            <div className='card-back'>
-              <p id='back-text'>{this.state.load}</p>
-            </div>
-          </div>
-        <button onClick={this.tweeted} id='tweet-quote-btn'>
-          <i className='fa fa-twitter'></i>
-          <a id='tweet-quote' href='https://twitter.com/intent/tweet'>Tweet Quote</a>
-        </button>
-        </div>
-      </div>
-    );
-  }
-};
-
-class Backdrop extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      background: 'whitesmoke',
-      colours: ['#16A085', '#27AE60', '#F39C12', '#E74C3C',
-               '#9B59B6', '#FB6964', '#BDBB99',
-               '#77B1A9', '#73A857']
-    };
-    this.changeBackground = this.changeBackground.bind(this);
-  }
-
-  changeBackground() {
-    const newBackground = getNewValue(this.state.background, this.state.colours);
-    this.setState({
-      background: newBackground
-    });
-  }
-
-  render() {
-    return (
-      <div className='backdrop' style={{ backgroundColor: this.state.background }}>
-        <QuoteBox changeBackground={this.changeBackground} />
-      </div>
-    );
-  }
-};
-
-export default Backdrop;
+export default Display;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Backdrop from './App';
+import Display from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Backdrop />, document.getElementById('root'));
+ReactDOM.render(<Display />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
